5290e6e46364af10aafbbb50446b2169d48b7758
BushCrescent <martian.halo@gmail.com>
2023-07-22 16:22:12 -0700

    sever setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// const htmlRoutes = require("./routes/htmlRoutes");
+// const apiRoutes = require("./routes/apiRoutes");
+
+//create server and port
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//set up body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//public asset access
+app.use(express.static("public"));
+// app.use(apiRoutes);
+// app.use(htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
